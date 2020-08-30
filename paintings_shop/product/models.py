30efb4aba53608ebf6855e8cbd8af330d9cffc7a
NAVEEN <navsoft18@gmail.com>
2020-08-30 18:38:43 +0530

    Add existing file

--- paintings_shop/product/models.py
+++ paintings_shop/product/models.py
@@ -6,3 +6,5 @@ class Product(models.Model):
      name=models.CharField(max_length=30)
      price = models.FloatField(default=0)
      categ_id=models.IntegerField(default=0)
+     picname = models.TextField(max_length=12, default="-")
+     picurl = models.TextField(default="-")
--- paintings_shop/product/views.py
+++ paintings_shop/product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from category.models import Category
 from .models import Product
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
@@ -20,8 +21,49 @@ def create_product(request):
         # newsid=request.POST.get('newscat')
         # tag=request.POST.get("tag")
 
-        b = Product(name=product_name, price=price, categ_id=product_cat)
-        b.save()
+        # b = Product(name=product_name, price=price, categ_id=product_cat)
+        # b.save()
+
+        try:
+
+            myfile=request.FILES['myfile']# myfile from the name of the html input
+            fs=FileSystemStorage()
+            filename=fs.save(myfile.name,myfile)
+            url=fs.url(filename)
+
+            if str(myfile.content_type).startswith("image"):
+
+
+                if myfile.size<5000000:
+                    # newsname = SubCat.objects.get(pk=newsid).name
+                    # ocatid=SubCat.objects.get(pk=newsid).catid#get catid from subcategory of news
+                    # b = News(name=newstitle, short_txt=newstxtshort, body_txt=newstxt, date=today,time=time, picname=filename,
+                    #      picurl=url, writer=request.user, catname=newsname,
+                    #      catid=newsid, show=0,ocatid=ocatid,tag=tag, rand = rand)
+                    # b.save()
+                    b = Product(name=product_name, price=price, categ_id=product_cat,picname=filename,picurl=url)
+                    b.save()
+
+                    #count of the newses with the ocatid(extracted from subcategory-previous)
+                    # count=len(News.objects.filter(ocatid=ocatid))
+                    # b=Cat.objects.get(pk=ocatid)#to update count in cat model
+                    # b.count=count
+                    # b.save()
+                    # return redirect('news_list')
+                else:
+                    error="File is Bigger than 1 MB"
+                    return render(request,'back/error.html',{'error':error})
+
+            else:
+                fs=FileSystemStorage()
+                fs.delete(filename)
+
+                error="Your File Not Supported"
+                return render(request,'back/error.html',{'error':error})
+
+        except:
+            error = "Please Input Your Image"
+            return render(request, 'back/error.html', {'error': error})
 
 
     return render(request, 'back/create_product.html', {'category': category})
