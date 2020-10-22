8af4be90336b4f9b1c8f188fa1c01d6ca46c7d0a
NAVEEN <navsoft18@gmail.com>
2020-10-23 00:34:04 +0530

    Add existing file

--- paintings_shop/home/views.py
+++ paintings_shop/home/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 import requests
 from django.views.decorators.csrf import csrf_exempt# i think:when connecting to other websites(like payment gateway)
 from product.models import Product
-
+from category.models import Category
 # Create your views here.
 @csrf_exempt
 def home(request):
 
     products = Product.objects.all()
     # categories = {}
+    #Categories
+    categories = Category.objects.all()
+    for cat in categories :
+        print(cat.name)
+
     return render(request, 'front/home-page.html',{'products':products})
