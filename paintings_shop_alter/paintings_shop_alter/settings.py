16331701fd94b350c74d28d0d4b5fca315f5bad7
NAVEEN <navsoft18@gmail.com>
2020-10-30 21:42:29 +0530

    Add existing file

--- paintings_shop_alter/paintings_shop_alter/settings.py
+++ paintings_shop_alter/paintings_shop_alter/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'navodoo1320@gmail.com'
-EMAIL_HOST_PASSWORD = 'Navodoo13@2020'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'navodoo1320@gmail.com'
